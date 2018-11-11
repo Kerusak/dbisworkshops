682d647d9672c0ce132a380aa66fe47e657bbbf1
Viktoria <0433vikla@gmail.com>
2018-11-12 00:41:43 +0200

    adding the sql files and erd schemas

---
+++ km-51/Meliukh_Viktoriia/project/Create.sql
@@ -0,0 +1,147 @@
+﻿/*==============================================================*/
+/* DBMS name:      ORACLE Version 12c                           */
+/* Created on:     11.11.2018 20:30:17                          */
+/*==============================================================*/
+
+
+alter table BOOKING
+   drop constraint FK_BOOKING_B_TO_C_CLASSROO;
+
+alter table BOOKING
+   drop constraint FK_BOOKING_B_TO_L_LESSON;
+
+alter table BOOKING
+   drop constraint FK_BOOKING_HAS_USER;
+
+drop index B_TO_C_FK;
+
+drop index B_TO_L_FK;
+
+drop index HAS_FK;
+
+drop table BOOKING cascade constraints;
+
+drop table CLASSROOM cascade constraints;
+
+drop table LESSON cascade constraints;
+
+drop table "USER" cascade constraints;
+
+/*==============================================================*/
+/* Table: BOOKING                                               */
+/*==============================================================*/
+create table BOOKING (
+   BOOKING_ID           VARCHAR2(32)          not null,
+   USER_ID              VARCHAR2(32)          not null,
+   LESSON_ID            VARCHAR2(32)          not null,
+   CLASSROOM_ID         VARCHAR2(32)          not null,
+   BOOKINGDATE          DATE                  not null,
+   constraint PK_BOOKING primary key (BOOKING_ID)
+);
+
+/*==============================================================*/
+/* Index: HAS_FK                                                */
+/*==============================================================*/
+create index HAS_FK on BOOKING (
+   USER_ID ASC
+);
+
+/*==============================================================*/
+/* Index: B_TO_L_FK                                             */
+/*==============================================================*/
+create index B_TO_L_FK on BOOKING (
+   LESSON_ID ASC
+);
+
+/*==============================================================*/
+/* Index: B_TO_C_FK                                             */
+/*==============================================================*/
+create index B_TO_C_FK on BOOKING (
+   CLASSROOM_ID ASC
+);
+
+/*==============================================================*/
+/* Table: CLASSROOM                                             */
+/*==============================================================*/
+create table CLASSROOM (
+   CLASSROOM_ID         VARCHAR2(32)          not null,
+   CLASSROOMNUM         INTEGER               not null,
+   HOUSINGNUMBER        INTEGER               not null,
+   NUMBEROFSEATS        INTEGER               not null,
+   MULTIMEDIA           INTEGER               not null,
+   constraint PK_CLASSROOM primary key (CLASSROOM_ID)
+);
+
+/*==============================================================*/
+/* Table: LESSON                                                */
+/*==============================================================*/
+create table LESSON (
+   LESSON_ID            VARCHAR2(32)                 not null,
+   LESSONNUMBER      INTEGER                      not null,
+   BTIME                VARCHAR2(5)                  not null,
+   ETIME                VARCHAR2(5)                  not null,
+   constraint PK_LESSON primary key (LESSON_ID)
+);
+
+/*==============================================================*/
+/* Table: "USER"                                                */
+/*==============================================================*/
+create table "USER" (
+   USER_ID              VARCHAR2(32)          not null,
+   FNAME                VARCHAR2(20)          not null,
+   MNAME                VARCHAR2(20)          not null,
+   LNAME                VARCHAR2(20)          not null,
+   EMAIL                VARCHAR2(20)          not null,
+   PASSWORD             VARCHAR2(20)          not null,
+   EMAILISCHECKED       INTEGER               not null,
+   constraint PK_USER primary key (USER_ID)
+);
+
+alter table BOOKING
+   add constraint FK_BOOKING_B_TO_C_CLASSROO foreign key (CLASSROOM_ID)
+      references CLASSROOM (CLASSROOM_ID);
+
+alter table BOOKING
+   add constraint FK_BOOKING_B_TO_L_LESSON foreign key (LESSON_ID)
+      references LESSON (LESSON_ID);
+
+alter table BOOKING
+   add constraint FK_BOOKING_HAS_USER foreign key (USER_ID)
+      references "USER" (USER_ID);
+
+
+/*------MANUALY ADDED CHECK CONSTRAINTS-----------*/
+
+
+alter table "USER"
+    add constraint check_f_name 
+    check(REGEXP_LIKE(FNAME, '[А-Яа-я]{1,20}', 'c'));
+  
+alter table "USER"
+    add constraint check_m_name 
+    check(REGEXP_LIKE(MNAME, '[А-Яа-я]{1,20}', 'c')); 
+  
+alter table "USER"
+    add constraint check_l_name 
+    check(REGEXP_LIKE(LNAME, '[А-Яа-я]{1,20}', 'c'));
+
+alter table "USER"
+    add constraint check_e_mail
+    check(REGEXP_LIKE(EMAIL, '^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$', 'i'));
+
+alter table "USER"
+    add constraint cehck_pass
+    check(REGEXP_LIKE(PASSWORD, '\d{1,20}'));
+
+alter table "USER"
+    add constraint check_e_checked
+    check(EMAILISCHECKED IN (0, 1));
+
+alter table "CLASSROOM"
+    add constraint uq_number_housing
+    UNIQUE  (CLASSROOMNUM, HOUSINGNUMBER);
+
+alter table lesson
+    add constraint uq_lesson_number
+    UNIQUE (lessonnumber);
+  
---
+++ km-51/Meliukh_Viktoriia/project/Populate.sql
@@ -0,0 +1,126 @@
+﻿/* INSERTS INTO CLASSROOM*/
+
+INSERT INTO classroom
+    values(sys_guid(),95,15,20,0);
+    
+INSERT INTO classroom
+    values(sys_guid(),96,15,25,0);
+    
+INSERT INTO classroom
+    values(sys_guid(),97,15,60,1);
+    
+INSERT INTO classroom
+    values(sys_guid(),93,15,20,1);
+
+INSERT INTO classroom
+    values(sys_guid(),100,15,35,1);
+    
+INSERT INTO classroom
+    values(sys_guid(),106,15,45,0);
+    
+INSERT INTO classroom
+    values(sys_guid(),105,15,75,1);
+    
+INSERT INTO classroom
+    values(sys_guid(),103,15,20,0);
+    
+/*LESSON*/
+INSERT INTO lesson
+    values(sys_guid(), 1, '9-30', '10-05');
+
+INSERT INTO lesson
+    values(sys_guid(), 2, '10-25', '10-05');
+    
+INSERT INTO lesson
+    values(sys_guid(), 3, '10-25', '12-00');
+    
+INSERT INTO lesson
+    values(sys_guid(), 4, '12.20', '13.55');
+    
+INSERT INTO lesson
+    values(sys_guid(), 5, '12.20', '13.55');
+    
+INSERT INTO lesson
+    values(sys_guid(), 6, '14.15', '15.50');
+    
+INSERT INTO lesson
+    values(sys_guid(), 7, '14.15', '15.50');
+    
+INSERT INTO lesson
+    values(sys_guid(), 8, '14.15', '15.50');
+
+/*USERS*/
+
+INSERT INTO "USER"
+    values(sys_guid(),'Иван', 'Иванович', 'Иванов', 'ivan@mail.com', ora_hash(1111), 1);
+    
+INSERT INTO "USER"
+    values(sys_guid(),'Васильев', 'Виктор', 'Григорьевич', 'nik@mail.com', ora_hash(1234), 1);
+    
+INSERT INTO "USER"
+    values(sys_guid(),'Иванченко','Ваня','Петрович','iv@mail.com', ora_hash(45679), 1);
+    
+INSERT INTO "USER"
+    values(sys_guid(),'Богомол','Универ','Медецинский','petro@mail.com', ora_hash(47532), 1);
+    
+INSERT INTO "USER"
+    values(sys_guid(),'Самбуров','Алексей','Гантченко','oma@mail.com', ora_hash(56832), 1);
+
+
+/*BOOKINGS*/
+
+INSERT INTO booking
+    values(
+        sys_guid(),
+        (SELECT USER_ID FROM "USER" WHERE email = 'ivan@mail.com'),
+        (SELECT LESSON_ID FROM lesson WHERE lesson.lessonnumber = 1 ),
+        (SELECT CLASSROOM_ID FROM classroom WHERE  classroom.classroomnum = 95 AND classroom.housingnumber = 15),
+        CURRENT_DATE -20);
+    
+INSERT INTO booking
+    values(
+        sys_guid(),
+        (SELECT USER_ID FROM "USER" WHERE email = 'nik@mail.com'),
+        (SELECT LESSON_ID FROM lesson WHERE lesson.lessonnumber = 2 ),
+        (SELECT CLASSROOM_ID FROM classroom WHERE  classroom.classroomnum = 96 AND classroom.housingnumber = 15),
+        CURRENT_DATE -19);
+
+INSERT INTO booking
+    values(
+        sys_guid(),
+        (SELECT USER_ID FROM "USER" WHERE email = 'iv@mail.com'),
+        (SELECT LESSON_ID FROM lesson WHERE lesson.lessonnumber = 1 ),
+        (SELECT CLASSROOM_ID FROM classroom WHERE  classroom.classroomnum = 100 AND classroom.housingnumber = 15),
+        CURRENT_DATE -18);
+        
+INSERT INTO booking
+    values(
+        sys_guid(),
+        (SELECT USER_ID FROM "USER" WHERE email = 'petro@mail.com'),
+        (SELECT LESSON_ID FROM lesson WHERE lesson.lessonnumber = 1 ),
+        (SELECT CLASSROOM_ID FROM classroom WHERE  classroom.classroomnum = 93 AND classroom.housingnumber = 15),
+        CURRENT_DATE -17);
+    
+INSERT INTO booking
+    values(
+        sys_guid(),
+        (SELECT USER_ID FROM "USER" WHERE email = 'oma@mail.com'),
+        (SELECT LESSON_ID FROM lesson WHERE lesson.lessonnumber = 1 ),
+        (SELECT CLASSROOM_ID FROM classroom WHERE  classroom.classroomnum = 106 AND classroom.housingnumber = 15),
+        CURRENT_DATE -16);
+        
+INSERT INTO booking
+    values(
+        sys_guid(),
+        (SELECT USER_ID FROM "USER" WHERE email = 'ivan@mail.com'),
+        (SELECT LESSON_ID FROM lesson WHERE lesson.lessonnumber = 1 ),
+        (SELECT CLASSROOM_ID FROM classroom WHERE  classroom.classroomnum = 103 AND classroom.housingnumber = 15),
+        CURRENT_DATE + 10);
+    
+    
+
+
+SELECT * FROM booking;
+SELECT * FROM classroom;
+SELECT * FROM "USER";
+SELECT * FROM lesson;
